50142c0ae36402c2fcb38330b8216d4f84348006
Daniil <krupnov_2001@mail.ru>
2025-04-19 02:48:03 +0300

    add home screen

---
+++ client/src/views/HomeView/HomeHeader.vue
@@ -0,0 +1,49 @@
+<template>
+    <header class="header-container">
+        <span>
+        <img src="../../assets/Logo-mini.png" alt="" />
+        <label>SQL Coding</label>
+        </span>
+        <button class="sign-up-btn" @click="$emit('open-auth', 'login')">Вход</button>
+    </header>
+</template>
+
+<script setup>
+  // Никаких реактивностей не нужно — кнопка просто эмитит событие
+</script>
+
+<style scoped lang="scss">
+.header-container {
+width: 100%;
+height: 10vh;
+display: flex;
+align-items: flex-start;
+justify-content: space-between;
+
+span {
+    display: flex;
+    align-items: center;
+}
+
+label {
+    font-size: 26px;
+    font-weight: 500;
+    transform: translateX(-10px);
+}
+
+button {
+    display: flex;
+    align-items: center;
+    height: 40px;
+    position: relative;
+    overflow: hidden;
+    z-index: 0;
+    background-color: var(--shadow-background);
+    padding: 5px 20px;
+    border-radius: 10px;
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    color: white;
+    cursor: pointer;
+}
+}
+</style>
---
+++ client/src/views/HomeView/HomeMain.vue
@@ -0,0 +1,50 @@
+<template>
+    <main class="main-container">
+      <img src="../../assets/big-logo.png" alt="">
+      <span>
+        <div class="main-label-text-title">SQL Coding</div>
+        <div class="main-label-text-semititle">SQL Coding - Просто. Чётко. Работает.</div>
+        <div class="main-label-text">Учись писать SQL-запросы быстро и просто.</div>
+        <div class="main-label-text">Пробуй, ошибайся, улучшай! </div>
+      </span>
+    </main>
+</template>
+
+<script setup>
+    // Никаких реактивностей не нужно — кнопка просто эмитит событие
+</script>
+
+<style scoped lang="scss">
+.main-container{
+width: 100%;
+height: 100%;
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: center;
+gap: 10px;
+
+    span{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+        .main-label-text-title{
+            font-size: 63px;
+            font-weight: 900;
+            padding: 0 20px;
+            background-color: rgba(214, 214, 214, 0.3);
+            border-radius: 10px;
+            border: 1px solid rgba(255, 255, 255, 0.1);
+            transform: translateX(-60px);
+        }
+        .main-label-text-semititle{
+            font-size: 37px;
+            transform: translateX(-30px);
+        }
+        .main-label-text{
+            font-size: 20px;
+        }
+    }
+}
+</style>
