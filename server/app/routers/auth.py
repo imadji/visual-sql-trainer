9678ba188773bd723097e256f291122483c23362
senks <yungync@gmail.com>
2025-04-19 01:10:22 +0300

    server

---
+++ server/app/routers/auth.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from .. import models, schemas
+from ..database import get_db
+
+router = APIRouter(prefix="/auth")
+
+
+@router.post("/login")
+def read_item(pd_user: schemas.UserPD, db: Session = Depends(get_db)):
+    db_user = (
+        db.query(models.UserDB)
+        .filter(models.UserDB.login == pd_user.login)
+        .first()
+    )
+    if not db_user:
+        return {"status": False}
+    elif db_user.password == pd_user.password:
+        return {"status": True}
+    else:
+        return {"status": False}
+
+
+@router.post("/reg")
+def read_item(pd_user: schemas.UserPD, db: Session = Depends(get_db)):
+    db_user = (
+        db.query(models.UserDB)
+        .filter(models.UserDB.login == pd_user.login)
+        .first()
+    )
+    if not db_user:
+        db_user = models.UserDB(
+            login=pd_user.login,
+            db_login=pd_user.login,
+            password=pd_user.password,
+            db_password=pd_user.password,
+        )
+        db.add(db_user)
+        db.commit()
+        return {"status": True}
+    return {"status": False}
