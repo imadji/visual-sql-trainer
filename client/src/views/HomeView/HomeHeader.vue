70bbabd98cc39255d694a5e85dace4523f6fc97d
Daniil <krupnov_2001@mail.ru>
2025-04-19 11:42:34 +0300

    add register form

--- client/src/views/HomeView/HomeHeader.vue
+++ client/src/views/HomeView/HomeHeader.vue
@@ -4,8 +4,10 @@
       <img src="../../assets/Logo-mini.png" alt="" />
       <label>SQL Coding</label>
     </span>
-    <button class="sign-up-btn" @click="$emit('open-auth', 'login')">Вход</button>
-    <button class="sign-up-btn" @click="$emit('open-auth', 'register')">Регистрация</button>
+    <div class="sign-container">
+      <button class="sign-up-btn" @click="$emit('open-auth', 'login')">Вход</button>
+      <button class="sign-up-btn" @click="$emit('open-auth', 'register')">Регистрация</button>
+    </div>
   </header>
 </template>
 
@@ -30,42 +32,48 @@
     transform: translateX(-10px);
   }
 
-  button {
+  .sign-container{
     display: flex;
-    align-items: center;
-    height: 40px;
-    position: relative;
-    overflow: hidden;
-    z-index: 0;
-    background-color: var(--shadow-background);
-    padding: 5px 20px;
-    border-radius: 10px;
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    color: white;
-    cursor: pointer;
+    flex-direction: row;
+    gap: 10px;
+    
+    button {
+      display: flex;
+      align-items: center;
+      height: 40px;
+      position: relative;
+      overflow: hidden;
+      z-index: 0;
+      background-color: var(--shadow-background);
+      padding: 5px 20px;
+      border-radius: 10px;
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      color: white;
+      cursor: pointer;
 
-    &::before {
-      content: "";
-      position: absolute;
-      top: 0;
-      left: -60%;
-      width: 40%;
-      height: 100%;
-      background: linear-gradient(
-        120deg,
-        rgba(255, 255, 255, 0) 0%,
+      &::before {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: -60%;
+        width: 40%;
+        height: 100%;
+        background: linear-gradient(
+          120deg,
+          rgba(255, 255, 255, 0) 0%,
         rgba(255, 255, 255, 0.4) 50%,
         rgba(255, 255, 255, 0) 100%
-      );
-      transform: skewX(-20deg);
-      animation: shine 3s ease-in-out infinite;
-      z-index: 1;
-      pointer-events: none;
+        );
+        transform: skewX(-20deg);
+        animation: shine 3s ease-in-out infinite;
+        z-index: 1;
+        pointer-events: none;
+      }
     }
-  }
-
-  @keyframes shine {
-    0% {
+    }
+    
+    @keyframes shine {
+      0% {
       left: -60%;
     }
     100% {
@@ -73,19 +81,16 @@
     }
   }
 
-  // button {
-  //     display: flex;
-  //     align-items: center;
-  //     height: 40px;
-  //     position: relative;
-  //     overflow: hidden;
-  //     z-index: 0;
-  //     background-color: var(--shadow-background);
-  //     padding: 5px 20px;
-  //     border-radius: 10px;
-  //     border: 1px solid rgba(255, 255, 255, 0.1);
-  //     color: white;
-  //     cursor: pointer;
-  // }
+  /* Small (sm) – устройства от 576px */
+}
+
+@media (max-width: 456px) {
+  .header-container {
+    .sign-container{
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+  }
 }
 </style>
